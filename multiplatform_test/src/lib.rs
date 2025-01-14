66bb3efd192886c97b4189c85cc8e338c2975f94
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-01-14 15:04:06 -0800

    fixup! style: fix all unexpected cfgs

--- multiplatform_test/src/lib.rs
+++ multiplatform_test/src/lib.rs
@@ -52,7 +52,10 @@ impl Platform {
             Platform::AsyncStd => quote! { #[async_std::test] },
             Platform::Dfir => quote! { #[dfir_rs::test] },
             Platform::Wasm => {
-                quote! { #[wasm_bindgen_test::wasm_bindgen_test] }
+                quote! {
+                    #[allow(unexpected_cfgs, reason = "cfg(wasm_bindgen_unstable_test_coverage)")]
+                    #[wasm_bindgen_test::wasm_bindgen_test]
+                }
             }
             Platform::EnvLogging | Platform::EnvTracing => Default::default(),
         }
