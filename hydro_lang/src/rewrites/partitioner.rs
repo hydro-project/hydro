85f341c3ac8ea9961caadbe100ad6f5cb17789c1
David Chu <davidchuyaya@gmail.com>
2025-02-19 22:25:45 +0000

    Partitioning modifies behavior of cluster.members()

--- hydro_lang/src/rewrites/partitioner.rs
+++ hydro_lang/src/rewrites/partitioner.rs
@@ -2,6 +2,9 @@
 use std::collections::HashMap;
 
 #[cfg(feature = "build")]
+use syn::visit_mut::{self, VisitMut};
+
+#[cfg(feature = "build")]
 use crate::ir::*;
 
 /// Fields that could be used for partitioning
@@ -20,13 +23,56 @@ pub struct Partitioner {
 // Placeholder ClusterId type
 pub struct Partitioned {}
 
+/// Don't expose partition members to the cluster
+#[cfg(feature = "build")]
+pub struct ClusterMembersReplace {
+    pub num_partitions: usize,
+}
+
+#[cfg(feature = "build")]
+impl VisitMut for ClusterMembersReplace {
+    fn visit_expr_mut(&mut self, expr: &mut syn::Expr) {
+        if let syn::Expr::Unsafe(unsafe_expr) = expr {
+            for stmt in &mut unsafe_expr.block.stmts {
+                if let syn::Stmt::Expr(stmt_expr, _) = stmt {
+                    if let syn::Expr::Call(call_expr) = stmt_expr {
+                        for arg in call_expr.args.iter_mut() {
+                            if let syn::Expr::Path(path_expr) = arg {
+                                for segment in path_expr.path.segments.iter_mut() {
+                                    let ident = segment.ident.to_string();
+                                    if ident.starts_with("__hydro_lang_cluster_ids_") {
+                                        let num_partitions = self.num_partitions;
+                                        let expr_content = std::mem::replace(expr, syn::Expr::PLACEHOLDER);
+                                        *expr = syn::parse_quote! { 
+                                            &#expr_content[0..#expr_content.len() / #num_partitions]
+                                        };
+                                        // Don't need to visit children
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        visit_mut::visit_expr_mut(self, expr);
+    }
+}
+
 #[cfg(feature = "build")]
 fn partition_node(node: &mut HydroNode, partitioner: &Partitioner, next_stmt_id: usize) {
     let Partitioner {
         nodes_to_partition,
         num_partitions,
-        ..
     } = partitioner;
+
+    node.visit_debug_expr(|expr| {
+        let mut visitor = ClusterMembersReplace {
+            num_partitions: *num_partitions,
+        };
+        visitor.visit_expr_mut(&mut expr.0);
+    });
 
     if let Some(partition_attr) = nodes_to_partition.get(&next_stmt_id) {
         println!("Partitioning node {} {}", next_stmt_id, node.print_root());
