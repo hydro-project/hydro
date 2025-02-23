d2b7c697fd76421cc1deb52e6ee7f198068cb750
Ryan Alameddine <rhalameddin@gmail.com>
2025-02-23 13:27:34 -0800

    feat(hydroflow): Re-introduced poll_futures.

--- hydro_lang/src/rewrites/persist_pullup.rs
+++ hydro_lang/src/rewrites/persist_pullup.rs
@@ -45,6 +45,28 @@ fn persist_pullup_node(
                 }
             }
 
+            HydroNode::PollFutures {
+                input: mb!(* HydroNode::Persist { inner: behind_persist, .. }),
+                metadata,
+            } => HydroNode::Persist {
+                inner: Box::new(HydroNode::PollFutures {
+                    input: behind_persist,
+                    metadata: metadata.clone(),
+                }),
+                metadata: metadata.clone(),
+            },
+
+            HydroNode::PollFuturesOrdered {
+                input: mb!(* HydroNode::Persist { inner: behind_persist, .. }),
+                metadata,
+            } => HydroNode::Persist {
+                inner: Box::new(HydroNode::PollFuturesOrdered {
+                    input: behind_persist,
+                    metadata: metadata.clone(),
+                }),
+                metadata: metadata.clone(),
+            },
+
             HydroNode::Map {
                 f,
                 input: mb!(* HydroNode::Persist { inner: behind_persist, .. }),
