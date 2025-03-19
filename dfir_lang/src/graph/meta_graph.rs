1d76389ce653c2b4be2bbe2b2bf2b604579a6d19
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-03-19 16:23:42 -0700

    fix for lints

--- dfir_lang/src/graph/meta_graph.rs
+++ dfir_lang/src/graph/meta_graph.rs
@@ -24,7 +24,7 @@ use super::{
     Varname, change_spans, get_operator_generics,
 };
 use crate::diagnostic::{Diagnostic, Level};
-use crate::pretty_span::{PrettyRowCol, PrettySpan, make_source_path_relative};
+use crate::pretty_span::{PrettyRowCol, PrettySpan};
 use crate::process_singletons;
 
 /// An abstract "meta graph" representation of a DFIR graph.
@@ -1023,10 +1023,12 @@ impl DfirGraph {
                                     let op_span = op_span.unwrap();
                                     break 'a format!(
                                         "loc_{}_{}_{}_{}_{}",
-                                        make_source_path_relative(&op_span.source_file().path())
-                                            .display()
-                                            .to_string()
-                                            .replace(|x: char| !x.is_ascii_alphanumeric(), "_"),
+                                        crate::pretty_span::make_source_path_relative(
+                                            &op_span.source_file().path()
+                                        )
+                                        .display()
+                                        .to_string()
+                                        .replace(|x: char| !x.is_ascii_alphanumeric(), "_"),
                                         op_span.start().line(),
                                         op_span.start().column(),
                                         op_span.end().line(),
