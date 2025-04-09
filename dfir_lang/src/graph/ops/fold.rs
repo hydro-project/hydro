4dea73fb9d3fdbed6fdb3cd198be0bdec63abd8b
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-04-09 14:58:24 -0700

    cleanup fold code

--- dfir_lang/src/graph/ops/fold.rs
+++ dfir_lang/src/graph/ops/fold.rs
@@ -1,11 +1,9 @@
 use quote::quote_spanned;
-use syn::{Expr, parse_quote_spanned};
 
 use super::{
     DelayType, OperatorCategory, OperatorConstraints, OperatorWriteOutput, Persistence, RANGE_0,
     RANGE_1, WriteContextArgs,
 };
-use crate::diagnostic::Diagnostic;
 
 /// > 1 input stream, 1 output stream
 ///
@@ -51,29 +49,98 @@ pub const FOLD: OperatorConstraints = OperatorConstraints {
     ports_out: None,
     input_delaytype_fn: |_| Some(DelayType::Stratum),
     write_fn: |wc @ &WriteContextArgs {
-                   op_span, arguments, ..
+                   root,
+                   context,
+                   df_ident,
+                   op_span,
+                   ident,
+                   is_pull,
+                   inputs,
+                   singleton_output_ident,
+                   work_fn,
+                   arguments,
+                   ..
                },
                diagnostics| {
         let init_fn = &arguments[0];
         let func = &arguments[1];
 
         let initializer_func_ident = wc.make_ident("initializer_func");
-        let init = parse_quote_spanned! {op_span=>
+        let init = quote_spanned! {op_span=>
             (#initializer_func_ident)()
         };
 
-        let OperatorWriteOutput {
-            write_prologue,
-            write_prologue_after,
-            write_iterator,
-            write_iterator_after,
-        } = fold_impl(wc, diagnostics, &init, func)?;
+        let [persistence] = wc.persistence_args_disallow_mutable(diagnostics);
+
+        let input = &inputs[0];
+        let accumulator_ident = wc.make_ident("accumulator");
+        let iterator_item_ident = wc.make_ident("iterator_item");
 
         let write_prologue = quote_spanned! {op_span=>
             #[allow(unused_mut, reason = "for if `Fn` instead of `FnMut`.")]
             let mut #initializer_func_ident = #init_fn;
 
-            #write_prologue
+            #[allow(clippy::redundant_closure_call)]
+            let #singleton_output_ident = #df_ident.add_state(::std::cell::RefCell::new(#init));
+        };
+        let write_prologue_after =wc
+        .persistence_as_state_lifespan(persistence)
+        .map(|lifespan| quote_spanned! {op_span=>
+            #[allow(clippy::redundant_closure_call)]
+            #df_ident.set_state_lifespan_hook(#singleton_output_ident, #lifespan, move |rcell| { rcell.replace(#init); });
+        }).unwrap_or_default();
+
+        let assign_accum_ident = quote_spanned! {op_span=>
+            #[allow(unused_mut)]
+            let mut #accumulator_ident = unsafe {
+                // SAFETY: handle from `#df_ident.add_state(..)`.
+                #context.state_ref_unchecked(#singleton_output_ident)
+            }.borrow_mut();
+        };
+        let iterator_foreach = quote_spanned! {op_span=>
+            #[inline(always)]
+            fn call_comb_type<Accum, Item>(
+                accum: &mut Accum,
+                item: Item,
+                func: impl Fn(&mut Accum, Item),
+            ) {
+                (func)(accum, item);
+            }
+            #[allow(clippy::redundant_closure_call)]
+            call_comb_type(&mut *#accumulator_ident, #iterator_item_ident, #func);
+        };
+
+        let write_iterator = if is_pull {
+            quote_spanned! {op_span=>
+                let #ident = {
+                    #assign_accum_ident
+
+                    #work_fn(|| #input.for_each(|#iterator_item_ident| {
+                        #iterator_foreach
+                    }));
+
+                    #[allow(clippy::clone_on_copy)]
+                    {
+                        ::std::iter::once(#work_fn(|| ::std::clone::Clone::clone(&*#accumulator_ident)))
+                    }
+                };
+            }
+        } else {
+            quote_spanned! {op_span=>
+                let #ident = #root::pusherator::for_each::ForEach::new(|#iterator_item_ident| {
+                    #assign_accum_ident
+
+                    #iterator_foreach
+                });
+            }
+        };
+
+        let write_iterator_after = if let Persistence::Static | Persistence::Tick = persistence {
+            quote_spanned! {op_span=>
+                #context.schedule_subgraph(#context.current_subgraph(), false);
+            }
+        } else {
+            Default::default()
         };
 
         Ok(OperatorWriteOutput {
@@ -84,98 +151,3 @@ pub const FOLD: OperatorConstraints = OperatorConstraints {
         })
     },
 };
-
-pub(crate) fn fold_impl(
-    wc @ &WriteContextArgs {
-        root,
-        context,
-        df_ident,
-        op_span,
-        ident,
-        is_pull,
-        inputs,
-        singleton_output_ident,
-        work_fn,
-        ..
-    }: &WriteContextArgs,
-    diagnostics: &mut Vec<Diagnostic>,
-    init: &Expr,
-    func: &Expr,
-) -> Result<OperatorWriteOutput, ()> {
-    let [persistence] = wc.persistence_args_disallow_mutable(diagnostics);
-
-    let input = &inputs[0];
-    let accumulator_ident = wc.make_ident("accumulator");
-    let iterator_item_ident = wc.make_ident("iterator_item");
-
-    let write_prologue = quote_spanned! {op_span=>
-        #[allow(clippy::redundant_closure_call)]
-        let #singleton_output_ident = #df_ident.add_state(::std::cell::RefCell::new(#init));
-    };
-    let write_prologue_after =wc
-        .persistence_as_state_lifespan(persistence)
-        .map(|lifespan| quote_spanned! {op_span=>
-            #[allow(clippy::redundant_closure_call)]
-            #df_ident.set_state_lifespan_hook(#singleton_output_ident, #lifespan, move |rcell| { rcell.replace(#init); });
-        }).unwrap_or_default();
-
-    let assign_accum_ident = quote_spanned! {op_span=>
-        #[allow(unused_mut)]
-        let mut #accumulator_ident = unsafe {
-            // SAFETY: handle from `#df_ident.add_state(..)`.
-            #context.state_ref_unchecked(#singleton_output_ident)
-        }.borrow_mut();
-    };
-    let iterator_foreach = quote_spanned! {op_span=>
-        #[inline(always)]
-        fn call_comb_type<Accum, Item>(
-            accum: &mut Accum,
-            item: Item,
-            func: impl Fn(&mut Accum, Item),
-        ) {
-            (func)(accum, item);
-        }
-        #[allow(clippy::redundant_closure_call)]
-        call_comb_type(&mut *#accumulator_ident, #iterator_item_ident, #func);
-    };
-
-    let write_iterator = if is_pull {
-        quote_spanned! {op_span=>
-            let #ident = {
-                #assign_accum_ident
-
-                #work_fn(|| #input.for_each(|#iterator_item_ident| {
-                    #iterator_foreach
-                }));
-
-                #[allow(clippy::clone_on_copy)]
-                {
-                    ::std::iter::once(#work_fn(|| ::std::clone::Clone::clone(&*#accumulator_ident)))
-                }
-            };
-        }
-    } else {
-        quote_spanned! {op_span=>
-            let #ident = #root::pusherator::for_each::ForEach::new(|#iterator_item_ident| {
-                #assign_accum_ident
-
-                #iterator_foreach
-            });
-        }
-    };
-
-    let write_iterator_after = if let Persistence::Static | Persistence::Tick = persistence {
-        quote_spanned! {op_span=>
-            #context.schedule_subgraph(#context.current_subgraph(), false);
-        }
-    } else {
-        Default::default()
-    };
-
-    Ok(OperatorWriteOutput {
-        write_prologue,
-        write_prologue_after,
-        write_iterator,
-        write_iterator_after,
-    })
-}
