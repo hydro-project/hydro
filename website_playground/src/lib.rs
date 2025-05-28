5adabf66701436cc706229582c45bcbc28574a4c
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-05-27 17:45:15 -0700

    fixup some WriteConfig refs

--- website_playground/src/lib.rs
+++ website_playground/src/lib.rs
@@ -98,6 +98,7 @@ pub fn compile_dfir(
     no_pull_push: bool,
     no_handoffs: bool,
     no_references: bool,
+    no_loops: bool,
     op_short_text: bool,
 ) -> JsValue {
     let write_config = WriteConfig {
@@ -106,6 +107,7 @@ pub fn compile_dfir(
         no_pull_push,
         no_handoffs,
         no_references,
+        no_loops,
         op_short_text,
         op_text_no_imports: false,
     };
