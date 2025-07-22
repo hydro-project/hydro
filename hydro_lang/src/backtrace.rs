f54732918809a219568a8b4eb9ef15cc0d6b22fe
Shadaj Laddad <shadajl@amazon.com>
2025-07-22 13:47:14 -0700

    feat(hydro_lang): capture stack traces for each IR node
    Because Hydro is staged, the stack traces capture the structure of the program, which is helpful for profiling / visualization.
    
    Fixes #1866

---
+++ hydro_lang/src/backtrace.rs
@@ -0,0 +1,40 @@
+use std::fmt::Debug;
+
+#[derive(Clone)]
+pub struct BacktraceElement {
+    pub fn_name: String,
+    pub filename: Option<String>,
+    pub lineno: Option<u32>,
+}
+
+impl Debug for BacktraceElement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.fn_name)
+    }
+}
+
+#[cfg(feature = "build")]
+#[inline(never)]
+pub(crate) fn get_backtrace(skip_count: usize) -> Vec<BacktraceElement> {
+    let backtrace = backtrace::Backtrace::new();
+    backtrace
+        .frames()
+        .iter()
+        .flat_map(|frame| frame.symbols())
+        .skip(6 + skip_count)
+        .take_while(|s| {
+            !s.name()
+                .is_some_and(|n| n.as_str().unwrap().contains("__rust_begin_short_backtrace"))
+        })
+        .map(|symbol| BacktraceElement {
+            fn_name: symbol.name().unwrap().to_string(),
+            filename: symbol.filename().map(|f| f.display().to_string()),
+            lineno: symbol.lineno(),
+        })
+        .collect()
+}
+
+#[cfg(not(feature = "build"))]
+pub(crate) fn get_backtrace() -> Vec<BacktraceElement> {
+    panic!();
+}
