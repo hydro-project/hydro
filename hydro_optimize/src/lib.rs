450ca58908034637bd8d65043b43e26fb5c7f5c9
David Chu <davidchuyaya@gmail.com>
2025-07-23 23:15:45 +0000

    Use good_lp instead of gurobi, removed references to the ILP feature

--- hydro_optimize/src/lib.rs
+++ hydro_optimize/src/lib.rs
@@ -1,17 +1,12 @@
-stageleft::stageleft_no_entry_crate!();
-
 pub mod debug;
+pub mod decouple_analysis;
 pub mod decoupler;
 pub mod deploy;
+pub mod deploy_and_analyze;
 pub mod parse_results;
 pub mod partitioner;
 pub mod repair;
 pub mod rewrites;
-
-#[cfg(feature = "ilp")]
-#[cfg_attr(docsrs, doc(cfg(feature = "ilp")))]
-pub mod decouple_analysis;
-pub mod deploy_and_analyze;
 
 #[cfg(test)]
 mod test_init {
