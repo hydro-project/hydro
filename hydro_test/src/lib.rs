600591de5d4c8a40f487fd298e4442e864eabb86
Shadaj Laddad <shadajl@amazon.com>
2025-07-31 17:12:36 -0700

    feat(hydro_lang,hydro_deploy)!: introduce support for dynamic external clients
    So far, Hydro has only supported deployments where there is a statically known number of single-connection external clients for sending / receiving data. This is not practical in production applications where external users will dynamically connect to the Hydro service.
    
    This takes the first step towards addressing this, by setting up the networking mechanism for handling dynamically connected clients and adding an API for sending/receiving data. Sending data is currently only supported for raw-`Bytes`, asymmetric Bincode is to come.
    
    This also changes the `source_external_bytes` APIs to return the raw `BytesMut` sent over the network, rather than adding a freezing step.
    
    Towards #1709

--- hydro_test/src/lib.rs
+++ hydro_test/src/lib.rs
@@ -2,12 +2,21 @@ stageleft::stageleft_no_entry_crate!();
 
 pub mod cluster;
 pub mod distributed;
+pub mod external_client;
 pub mod local;
 
 #[doc(hidden)]
 #[cfg(doctest)]
 mod docs {
     include_mdtests::include_mdtests!("docs/docs/hydro/**/*.md*");
+}
+
+#[ctor::ctor]
+fn init_rewrites() {
+    stageleft::add_private_reexport(
+        vec!["tokio_util", "codec", "lines_codec"],
+        vec!["tokio_util", "codec"],
+    );
 }
 
 #[cfg(test)]
