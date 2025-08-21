9d25aa5721787680d901d543c9511f16b086bec8
Joe Hellerstein <jmh@berkeley.edu>
2025-08-20 22:25:00 -0700

    remove stray files

--- test_sizes.rs
+++
@@ -1,8 +0,0 @@
-use std::mem::size_of;
-
-use hydro_lang::ir::{HydroLeaf, HydroNode};
-
-fn main() {
-    println!("HydroNode size: {}", size_of::<HydroNode>());
-    println!("HydroLeaf size: {}", size_of::<HydroLeaf>());
-}
