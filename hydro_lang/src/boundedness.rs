cb5818ee4ee3415ff3028e474d5606041f5160a8
Shadaj Laddad <shadajl@amazon.com>
2025-08-27 14:10:41 -0700

    feat(hydro_lang)!: refine types when values of a `KeyedSingleton` are immutable
    There are many cases where once a value is released into a `KeyedSingleton`, it will never be changes. In such cases, we can permit APIs like `.entries` even if the set of entries is growing.
    
    We use this type to improve the API surface for looking up values for a request. Now, a `KeyedSingleton` of requests can look up values from another `KeyedSingleton`.

--- hydro_lang/src/boundedness.rs
+++ hydro_lang/src/boundedness.rs
@@ -1,10 +1,12 @@
 use sealed::sealed;
 
+use crate::keyed_singleton::{KeyedSingletonBound, ValueBounded};
+
 /// A marker trait indicating whether a stream’s length is bounded (finite) or unbounded (potentially infinite).
 ///
 /// Implementors of this trait use it to signal the boundedness property of a stream.
 #[sealed]
-pub trait Boundedness {}
+pub trait Boundedness: KeyedBoundFoldLike {}
 
 /// Marks the stream as being unbounded, which means that it is not
 /// guaranteed to be complete in finite time.
@@ -19,3 +21,19 @@ pub enum Bounded {}
 
 #[sealed]
 impl Boundedness for Bounded {}
+
+/// Helper trait that determines the boundedness for the result of keyed aggregations.
+#[sealed::sealed]
+pub trait KeyedBoundFoldLike {
+    type KeyedBound: KeyedSingletonBound;
+}
+
+#[sealed::sealed]
+impl KeyedBoundFoldLike for Unbounded {
+    type KeyedBound = Unbounded;
+}
+
+#[sealed::sealed]
+impl KeyedBoundFoldLike for Bounded {
+    type KeyedBound = ValueBounded<Bounded>;
+}
