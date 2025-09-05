5451bacdf5f04bb97ff4a7266aed224a026f504c
Shadaj Laddad <shadajl@amazon.com>
2025-09-05 11:23:08 -0700

    refactor(hydro_lang)!: move `rewrites` module under `builder`
    This module will soon go away entirely, since we will no longer rely on the `persist_pullup` rewrite for translating between Flo / batched-DFIR semantics. But in the meantime, we can at least move it from the top-level namespace.

--- hydro_optimize/src/deploy_and_analyze.rs
+++ hydro_optimize/src/deploy_and_analyze.rs
@@ -4,12 +4,12 @@ use std::time::Duration;
 use hydro_deploy::Deployment;
 use hydro_lang::builder::deploy::DeployResult;
 use hydro_lang::builder::ir::{HydroNode, HydroRoot, deep_clone, traverse_dfir};
+use hydro_lang::builder::rewrites::persist_pullup::persist_pullup;
 use hydro_lang::builder::{FlowBuilder, RewriteIrFlowBuilder};
 use hydro_lang::deploy::HydroDeploy;
 use hydro_lang::deploy::deploy_graph::DeployCrateWrapper;
 use hydro_lang::internal_constants::{COUNTER_PREFIX, CPU_USAGE_PREFIX};
 use hydro_lang::location::LocationId;
-use hydro_lang::rewrites::persist_pullup::persist_pullup;
 use tokio::sync::mpsc::UnboundedReceiver;
 
 use crate::decouple_analysis::decouple_analysis;
