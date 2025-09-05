c1f173097fe03b8e99b5175109f1911f1d4396f8
Shadaj Laddad <shadajl@amazon.com>
2025-09-05 10:57:21 -0700

    refactor(hydro_lang)!: move `ir` module under `builder`
    Most Hydro users don't need to interact with the `ir`, and its primary consumer is the compilation logic inside `builder`.

--- hydro_optimize/src/deploy_and_analyze.rs
+++ hydro_optimize/src/deploy_and_analyze.rs
@@ -3,11 +3,11 @@ use std::time::Duration;
 
 use hydro_deploy::Deployment;
 use hydro_lang::builder::deploy::DeployResult;
+use hydro_lang::builder::ir::{HydroNode, HydroRoot, deep_clone, traverse_dfir};
 use hydro_lang::builder::{FlowBuilder, RewriteIrFlowBuilder};
 use hydro_lang::deploy::HydroDeploy;
 use hydro_lang::deploy::deploy_graph::DeployCrateWrapper;
 use hydro_lang::internal_constants::{COUNTER_PREFIX, CPU_USAGE_PREFIX};
-use hydro_lang::ir::{HydroNode, HydroRoot, deep_clone, traverse_dfir};
 use hydro_lang::location::LocationId;
 use hydro_lang::rewrites::persist_pullup::persist_pullup;
 use tokio::sync::mpsc::UnboundedReceiver;
