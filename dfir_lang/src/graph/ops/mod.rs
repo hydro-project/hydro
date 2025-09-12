a0c2f86382574cd89e71d010abb7140ecf9d5ef7
Mingwei Samuel <mingwes@amazon.com>
2025-09-11 21:06:50 -0700

    refactor: make dfir use async Sinks via `sinktool`s [ci-bench]
    Pusherators are no longer used

--- dfir_lang/src/graph/ops/mod.rs
+++ dfir_lang/src/graph/ops/mod.rs
@@ -175,7 +175,7 @@ pub fn identity_write_iterator_fn(
         let output = &outputs[0];
         quote_spanned! {op_span=>
             let #ident = {
-                fn check_output<Push: #root::pusherator::Pusherator<Item = Item>, Item>(push: Push) -> impl #root::pusherator::Pusherator<Item = Item> { push }
+                fn check_output<Push: #root::futures::sink::Sink<Item>, Item>(push: Push) -> impl #root::futures::sink::Sink<Item> { push }
                 check_output::<_, #generic_type>(#output)
             };
         }
@@ -223,7 +223,7 @@ pub fn null_write_iterator_fn(
         quote_spanned! {op_span=>
             #[allow(clippy::let_unit_value)]
             let _ = (#(#outputs),*);
-            let #ident = #root::pusherator::null::Null::<#iter_type>::new();
+            let #ident = #root::sinktools::for_each::ForEach::new::<#iter_type>(::std::mem::drop::<#iter_type>);
         }
     }
 }
