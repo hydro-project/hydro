a5def5c01ee242eaf8a2aef384dd04f3399b3ea7
Shadaj Laddad <shadajl@amazon.com>
2025-09-15 15:14:55 -0700

    docs(hydro_lang): initial docs for `deploy` module

--- hydro_lang/src/live_collections/stream/networking.rs
+++ hydro_lang/src/live_collections/stream/networking.rs
@@ -389,6 +389,7 @@ impl<'a, T, L, B: Boundedness, O, R> Stream<T, Cluster<'a, L>, B, O, R> {
     /// # let workers: Cluster<()> = flow.cluster::<()>();
     /// # let numbers: Stream<_, Cluster<_>, _> = workers.source_iter(q!(vec![1]));
     /// numbers.send_bincode(&process).values() // Stream<i32, ..., NoOrder>
+    /// //
     /// # }, |mut stream| async move {
     /// // if there are 4 members in the cluster, we should receive 4 elements
     /// // 1, 1, 1, 1
