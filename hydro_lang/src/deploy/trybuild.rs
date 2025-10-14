c42ccda572ac3576cddb359554f291cd83941a02
Shadaj Laddad <shadajl@amazon.com>
2025-10-14 11:55:20 -0400

    feat(hydro_lang): share `__staged` across trybuild targets
    Previously, each trybuild target (in test-mode) would have its own copy of the `__staged` module containing the entire sources of the original crate. This creates a huge compilation burden that is repeated on every deployment.
    
    With an accompanying change in Stageleft, we are now able to share the `__staged` module in the common `lib.rs`. This also moves the simulation dylibs to be compiled as an example so that they do not conflict with the caching of the shared `lib.rs`.

--- hydro_lang/src/deploy/trybuild.rs
+++ hydro_lang/src/deploy/trybuild.rs
@@ -3,6 +3,7 @@ use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 
 use dfir_lang::graph::DfirGraph;
+use proc_macro2::Span;
 use sha2::{Digest, Sha256};
 use stageleft::internal::quote;
 use syn::visit_mut::VisitMut;
@@ -67,38 +68,30 @@ pub fn create_graph_trybuild(
 
     let generated_code = compile_graph_trybuild(graph, extra_stmts, crate_name.clone(), is_test);
 
-    let inlined_staged: syn::File = if is_test {
+    let inlined_staged = if is_test {
         let gen_staged = stageleft_tool::gen_staged_trybuild(
             &path!(source_dir / "src" / "lib.rs"),
             &path!(source_dir / "Cargo.toml"),
             crate_name.clone(),
-            is_test,
+            Some("hydro___test".to_string()),
         );
 
-        syn::parse_quote! {
-            #[allow(
+        Some(prettyplease::unparse(&syn::parse_quote! {
+            #![allow(
                 unused,
                 ambiguous_glob_reexports,
                 clippy::suspicious_else_formatting,
                 unexpected_cfgs,
                 reason = "generated code"
             )]
-            pub mod __staged {
-                #gen_staged
-            }
-        }
+
+            #gen_staged
+        }))
     } else {
-        let crate_name_ident = syn::Ident::new(crate_name, proc_macro2::Span::call_site());
-        syn::parse_quote!(
-            pub use #crate_name_ident::__staged;
-        )
+        None
     };
 
-    let source = prettyplease::unparse(&syn::parse_quote! {
-        #generated_code
-
-        #inlined_staged
-    });
+    let source = prettyplease::unparse(&generated_code);
 
     let hash = format!("{:X}", Sha256::digest(&source))
         .chars()
@@ -120,6 +113,44 @@ pub fn create_graph_trybuild(
     {
         let _concurrent_test_lock = CONCURRENT_TEST_LOCK.lock().unwrap();
         write_atomic(source.as_ref(), &out_path).unwrap();
+    }
+
+    if let Some(inlined_staged) = inlined_staged {
+        let staged_path = path!(project_dir / "src" / format!("__staged.rs"));
+        {
+            let _concurrent_test_lock = CONCURRENT_TEST_LOCK.lock().unwrap();
+            write_atomic(inlined_staged.as_bytes(), &staged_path).unwrap();
+        }
+    }
+
+    let crate_name_ident = syn::Ident::new(&crate_name, Span::call_site());
+    let lib_path = path!(project_dir / "src" / format!("lib.rs"));
+    {
+        let _concurrent_test_lock = CONCURRENT_TEST_LOCK.lock().unwrap();
+        write_atomic(
+            prettyplease::unparse(&syn::parse_quote! {
+                #![allow(unused_imports, unused_crate_dependencies, missing_docs, non_snake_case)]
+
+                #[cfg(feature = "hydro___test")]
+                pub mod __staged;
+
+                #[cfg(not(feature = "hydro___test"))]
+                pub use #crate_name_ident::__staged;
+            })
+            .as_bytes(),
+            &lib_path,
+        )
+        .unwrap();
+    }
+
+    let build_rs_path = path!(project_dir / "build.rs");
+    {
+        let _concurrent_test_lock = CONCURRENT_TEST_LOCK.lock().unwrap();
+        write_atomic(
+            b"fn main() { println!(\"cargo::rerun-if-env-changed=TRYBUILD_LIB_NAME\"); println!(\"cargo::rustc-check-cfg=cfg(__hydro_sim)\"); if std::env::var(\"TRYBUILD_LIB_NAME\").is_ok() { println!(\"cargo:rustc-cfg=__hydro_sim\"); } }",
+            &build_rs_path,
+        )
+        .unwrap();
     }
 
     if is_test {
@@ -165,10 +196,14 @@ pub fn compile_graph_trybuild(
         .visit_expr_mut(&mut dfir_expr);
     }
 
+    let trybuild_crate_name = format!("{}_hydro_trybuild", crate_name);
+    let trybuild_crate_name_ident = syn::Ident::new(&trybuild_crate_name, Span::call_site());
+
     let source_ast: syn::File = syn::parse_quote! {
         #![allow(unused_imports, unused_crate_dependencies, missing_docs, non_snake_case)]
         use hydro_lang::prelude::*;
         use hydro_lang::runtime_support::dfir_rs as __root_dfir_rs;
+        pub use #trybuild_crate_name_ident::__staged;
 
         #[allow(unused)]
         fn __hydro_runtime<'a>(__hydro_lang_trybuild_cli: &'a hydro_lang::runtime_support::dfir_rs::util::deploy::DeployPorts<hydro_lang::__staged::deploy::deploy_runtime::HydroMeta>) -> hydro_lang::runtime_support::dfir_rs::scheduled::graph::Dfir<'a> {
@@ -295,7 +330,25 @@ pub fn create_trybuild()
         project_lock.lock()?;
 
         let manifest_toml = toml::to_string(&project.manifest)?;
-        write_atomic(manifest_toml.as_ref(), &path!(project.dir / "Cargo.toml"))?;
+        let manifest_with_example = format!(
+            "{}\n\n[[example]]\nname = \"sim-dylib\"\ncrate-type = [\"dylib\"]",
+            manifest_toml
+        );
+        write_atomic(
+            manifest_with_example.as_ref(),
+            &path!(project.dir / "Cargo.toml"),
+        )?;
+
+        let examples_folder = path!(project.dir / "examples");
+        fs::create_dir_all(&examples_folder)?;
+        write_atomic(
+            prettyplease::unparse(&syn::parse_quote! {
+                #![allow(unused_imports, unused_crate_dependencies, missing_docs, non_snake_case)]
+                include!(std::concat!(env!("TRYBUILD_LIB_NAME"), ".rs"));
+            })
+            .as_bytes(),
+            &path!(project.dir / "examples" / "sim-dylib.rs"),
+        )?;
 
         let workspace_cargo_lock = path!(project.workspace / "Cargo.lock");
         if workspace_cargo_lock.exists() {
