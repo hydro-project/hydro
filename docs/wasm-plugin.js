68e31400005d0d6ba816025c2a3fb32087c28906
Joe Hellerstein <jmhwork@amazon.com>
2025-10-20 23:04:29 -0700

    docs: add local shim and webpack alias for @hydro-project/hydroscope to fix CI docs build; gate real package behind LOAD_HYDROSCOPE=1

--- docs/wasm-plugin.js
+++ docs/wasm-plugin.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = function (context, options) {
   return {
     name: 'wasm-docusuarus-plugin',
     // eslint-disable-next-line
     configureWebpack(config, isServer, utils) {
+      const useRealHydroscope = process.env.LOAD_HYDROSCOPE === '1';
       return {
         experiments: {
           asyncWebAssembly: !isServer,
@@ -15,14 +18,18 @@ module.exports = function (context, options) {
             },
           ] : []
         },
-        ...(process.env.LOAD_PLAYGROUND !== "1" ? {
-          resolve: {
-            alias: {
+        resolve: {
+          alias: {
+            ...(process.env.LOAD_PLAYGROUND !== "1" ? {
               "website_playground/website_playground_bg.wasm": false,
               "website_playground/website_playground_bg.js": false
-            }
+            } : {}),
+            // When LOAD_HYDROSCOPE is not set, alias the package to a local shim
+            ...(!useRealHydroscope ? {
+              "@hydro-project/hydroscope": path.resolve(__dirname, 'src/shims/hydroscope.tsx')
+            } : {})
           }
-        } : {})
+        }
       };
     },
   };
