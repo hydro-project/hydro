03ee9bf4359b2b0f8ad920ed276b3dd83db8caab
Joe Hellerstein <jmhwork@amazon.com>
2025-10-21 11:19:02 -0700

    docs: remove unnecessary hydroscope shim
    @hydro-project/hydroscope is already a declared npm dependency in
    package.json, so it will be available after npm install. The shim
    and webpack alias were unnecessary.

--- docs/wasm-plugin.js
+++ docs/wasm-plugin.js
@@ -1,11 +1,8 @@
-const path = require("path");
-
 module.exports = function (context, options) {
   return {
     name: "wasm-docusuarus-plugin",
     // eslint-disable-next-line
     configureWebpack(config, isServer, utils) {
-      const useRealHydroscope = process.env.LOAD_HYDROSCOPE === "1";
       return {
         experiments: {
           asyncWebAssembly: !isServer,
@@ -26,15 +23,6 @@ module.exports = function (context, options) {
               ? {
                   "website_playground/website_playground_bg.wasm": false,
                   "website_playground/website_playground_bg.js": false,
-                }
-              : {}),
-            // When LOAD_HYDROSCOPE is not set, alias the package to a local shim
-            ...(!useRealHydroscope
-              ? {
-                  "@hydro-project/hydroscope": path.resolve(
-                    __dirname,
-                    "src/shims/hydroscope.tsx"
-                  ),
                 }
               : {}),
           },
