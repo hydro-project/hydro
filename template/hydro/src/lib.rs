6fb60b53d47f48b286073695abceaca36684c3cf
Shadaj Laddad <shadajl@amazon.com>
2025-10-31 10:41:10 -0700

    docs: new template and walkthrough

--- template/hydro/src/lib.rs
+++ template/hydro/src/lib.rs
@@ -1,13 +1,40 @@
-stageleft::stageleft_no_entry_crate!();
+#[cfg(stageleft_runtime)]
+hydro_lang::setup!();
 
-pub mod first_ten;
-pub mod first_ten_cluster;
-pub mod first_ten_distributed;
+use hydro_lang::prelude::*;
+
+pub struct EchoServer;
+pub fn echo_capitalize<'a>(
+    input: Stream<String, Process<'a, EchoServer>>,
+) -> Stream<String, Process<'a, EchoServer>> {
+    input.map(q!(|s| s.to_uppercase()))
+}
 
 #[cfg(test)]
-mod test_init {
-    #[ctor::ctor]
-    fn init() {
-        hydro_lang::compile::init_test();
+mod tests {
+    use hydro_lang::prelude::*;
+
+    #[test]
+    fn test_echo_capitalize() {
+        let flow = FlowBuilder::new();
+        let node = flow.process();
+        let external = flow.external::<()>();
+
+        let (in_port, input) = node.source_external_bincode(&external);
+        let out_port = super::echo_capitalize(input).send_bincode_external(&external);
+
+        flow.sim().exhaustive(async |mut compiled| {
+            let in_port = compiled.connect(&in_port);
+            let out_port = compiled.connect(&out_port);
+
+            compiled.launch();
+
+            in_port.send("hello".to_string()).unwrap();
+            in_port.send("world".to_string()).unwrap();
+
+            out_port
+                .assert_yields_only(["HELLO".to_string(), "WORLD".to_string()])
+                .await;
+        });
     }
 }
