6fa3ef9e20398099f904317a1d2f9a7f46a4b619
Shadaj Laddad <shadajl@amazon.com>
2025-11-03 15:51:57 -0800

    docs: new template and walkthrough

---
+++ template/hydro/examples/echo_local.rs
@@ -0,0 +1,24 @@
+use hydro_deploy::Deployment;
+use hydro_lang::location::{Location, NetworkHint};
+use hydro_lang::prelude::*;
+use tokio_util::codec::LinesCodec;
+
+#[tokio::main]
+async fn main() {
+    let mut deployment = Deployment::new();
+
+    let flow = hydro_lang::compile::builder::FlowBuilder::new();
+    let process = flow.process();
+    let external = flow.external::<()>();
+
+    let (port, input, output) =
+        process.bind_single_client::<_, _, LinesCodec>(&external, NetworkHint::TcpPort(Some(4000)));
+    output.complete(hydro_template::echo_capitalize(input));
+
+    let _nodes = flow
+        .with_process(&process, deployment.Localhost())
+        .with_external(&external, deployment.Localhost())
+        .deploy(&mut deployment);
+
+    deployment.run_ctrl_c().await.unwrap();
+}
