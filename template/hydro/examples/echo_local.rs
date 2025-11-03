0c07addfa9b208b0b443a3cc8e324a671743955b
Shadaj Laddad <shadajl@amazon.com>
2025-11-03 10:27:24 -0800

    docs: new template and walkthrough

---
+++ template/hydro/examples/echo_local.rs
@@ -0,0 +1,37 @@
+use futures::sink::SinkExt;
+use hydro_deploy::Deployment;
+use hydro_lang::location::Location;
+use hydro_lang::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    let mut deployment = Deployment::new();
+
+    let flow = hydro_lang::compile::builder::FlowBuilder::new();
+    let process = flow.process();
+    let external = flow.external::<()>();
+
+    let (in_port, input) = process.source_external_bincode(&external);
+    hydro_template::echo_capitalize(input).for_each(q!(|s| {
+        println!("Echoed capitalized: {}", s);
+    }));
+
+    let nodes = flow
+        .with_process(&process, deployment.Localhost())
+        .with_external(&external, deployment.Localhost())
+        .deploy(&mut deployment);
+
+    deployment.deploy().await.unwrap();
+
+    let mut external_in = nodes.connect(in_port).await;
+
+    deployment.start().await.unwrap();
+
+    for line in std::io::stdin().lines() {
+        let msg = line.unwrap();
+        if msg.is_empty() {
+            break;
+        }
+        external_in.send(msg).await.unwrap();
+    }
+}
