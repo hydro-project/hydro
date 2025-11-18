86bf65b23bb105f44480731a3044543667e7b979
Mingwei Samuel <mingwes@amazon.com>
2025-11-18 14:21:52 -0800

    anti_join, shj

--- dfir_rs/src/compiled/pull/anti_join.rs
+++ dfir_rs/src/compiled/pull/anti_join.rs
@@ -1,72 +1,147 @@
-use itertools::Either;
+use std::pin::Pin;
+use std::task::{Context, Poll, ready};
+
+use futures::Stream;
+use futures::stream::FusedStream;
+use pin_project_lite::pin_project;
 use rustc_hash::FxHashSet;
 
-/// A custom [`Iterator`] for the `anti_join` operator.
-pub struct AntiJoin<'a, Key, V, Ipos>
-where
-    Key: Eq + std::hash::Hash + Clone,
-    V: Eq + std::hash::Hash + Clone,
-    Ipos: Iterator<Item = (Key, V)>,
-{
-    input_pos: Ipos,
-    neg_state: &'a mut FxHashSet<Key>,
-    pos_state: &'a mut FxHashSet<(Key, V)>,
-}
+pin_project! {
+    /// Special stream for the `anti_join` operator when `[pos]` is persisted.
+    #[must_use = "streams do nothing unless polled"]
+    pub struct AntiJoinPersist<'a, StPos, StNeg, Key, Val> {
+        #[pin]
+        stream_pos: StPos,
+        #[pin]
+        stream_neg: StNeg,
 
-impl<Key, V, Ipos> Iterator for AntiJoin<'_, Key, V, Ipos>
-where
-    Key: Eq + std::hash::Hash + Clone,
-    V: Eq + std::hash::Hash + Clone,
-    Ipos: Iterator<Item = (Key, V)>,
-{
-    type Item = (Key, V);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        for item in self.input_pos.by_ref() {
-            if !self.neg_state.contains(&item.0) && !self.pos_state.contains(&item) {
-                self.pos_state.insert(item.clone());
-                return Some(item);
-            }
-        }
-
-        None
+        state_pos: &'a mut Vec<(Key, Val)>,
+        state_neg: &'a mut FxHashSet<Key>,
+        replay_idx: usize,
     }
 }
 
-/// Creates a custom [`Iterator`] for the `anti_join` operator, replaying if `new_tick`.
-pub fn anti_join_into_iter<'a, Key, V, Ipos>(
-    input_pos: Ipos,
-    state_neg: &'a mut FxHashSet<Key>,
-    state_pos: &'a mut FxHashSet<(Key, V)>,
-    new_tick: bool,
-) -> impl 'a + Iterator<Item = (Key, V)>
+impl<'a, StPos, StNeg, Key, Val> AntiJoinPersist<'a, StPos, StNeg, Key, Val>
 where
+    StPos: Stream<Item = (Key, Val)>,
+    StNeg: FusedStream<Item = Key>,
     Key: Eq + std::hash::Hash + Clone,
-    V: Eq + std::hash::Hash + Clone,
-    Ipos: 'a + Iterator<Item = (Key, V)>,
+    Val: Eq + std::hash::Hash + Clone,
 {
-    if new_tick {
-        Either::Left(
-            state_pos
-                .iter()
-                .filter(|kv| !state_neg.contains(&kv.0))
-                .cloned()
-                .collect::<Vec<_>>()
-                .into_iter()
-                .chain(input_pos.filter_map(|kv| {
-                    if !state_neg.contains(&kv.0) {
-                        state_pos.insert(kv.clone());
-                        Some(kv)
-                    } else {
-                        None
-                    }
-                })),
-        )
-    } else {
-        Either::Right(AntiJoin {
-            input_pos,
-            neg_state: state_neg,
-            pos_state: state_pos,
-        })
+    pub fn new(
+        stream_pos: StPos,
+        stream_neg: StNeg,
+        state_pos: &'a mut Vec<(Key, Val)>,
+        state_neg: &'a mut FxHashSet<Key>,
+        replay_idx: usize,
+    ) -> Self {
+        debug_assert!(replay_idx <= state_pos.len());
+
+        Self {
+            stream_pos,
+            stream_neg,
+            state_pos,
+            state_neg,
+            replay_idx,
+        }
+    }
+}
+
+impl<'a, StPos, StNeg, Key, Val> Stream for AntiJoinPersist<'a, StPos, StNeg, Key, Val>
+where
+    StPos: Stream<Item = (Key, Val)>,
+    StNeg: FusedStream<Item = Key>,
+    Key: Eq + std::hash::Hash + Clone,
+    Val: Eq + std::hash::Hash + Clone,
+{
+    type Item = (Key, Val);
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+
+        // First, get all negative.
+        while let Some(neg_item) = ready!(this.stream_neg.as_mut().poll_next(cx)) {
+            this.state_neg.insert(neg_item);
+        }
+
+        if let Some(item) = this.state_pos.get(*this.replay_idx) {
+            // Second, replay.
+            *this.replay_idx += 1;
+            Poll::Ready(Some(item.clone()))
+        } else {
+            // Third, stream, filter, and store positive.
+            debug_assert_eq!(this.state_pos.len(), *this.replay_idx);
+
+            while let Some(item) = ready!(this.stream_pos.as_mut().poll_next(cx)) {
+                if !this.state_neg.contains(&item.0) {
+                    // New item
+                    this.state_pos.push(item.clone());
+                    *this.replay_idx += 1;
+
+                    return Poll::Ready(Some(item));
+                }
+            }
+
+            // Done
+            Poll::Ready(None)
+        }
+    }
+}
+
+pin_project! {
+    /// Special stream for the `anti_join` operator when `[pos]` is not persisted.
+    #[must_use = "streams do nothing unless polled"]
+    pub struct AntiJoin<'a, StPos, StNeg, Key> {
+        #[pin]
+        stream_pos: StPos,
+        #[pin]
+        stream_neg: StNeg,
+
+        state_neg: &'a mut FxHashSet<Key>,
+    }
+}
+
+impl<'a, StPos, StNeg, Key, Val> AntiJoin<'a, StPos, StNeg, Key>
+where
+    StPos: Stream<Item = (Key, Val)>,
+    StNeg: FusedStream<Item = Key>,
+    Key: Eq + std::hash::Hash + Clone,
+    Val: Eq + std::hash::Hash + Clone,
+{
+    pub fn new(stream_pos: StPos, stream_neg: StNeg, state_neg: &'a mut FxHashSet<Key>) -> Self {
+        Self {
+            stream_pos,
+            stream_neg,
+            state_neg,
+        }
+    }
+}
+
+impl<'a, StPos, StNeg, Key, Val> Stream for AntiJoin<'a, StPos, StNeg, Key>
+where
+    StPos: Stream<Item = (Key, Val)>,
+    StNeg: FusedStream<Item = Key>,
+    Key: Eq + std::hash::Hash + Clone,
+    Val: Eq + std::hash::Hash + Clone,
+{
+    type Item = (Key, Val);
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+
+        // First, get all negative.
+        while let Some(neg_item) = ready!(this.stream_neg.as_mut().poll_next(cx)) {
+            this.state_neg.insert(neg_item);
+        }
+
+        // Then stream & filter positive.
+        while let Some(item) = ready!(this.stream_pos.as_mut().poll_next(cx)) {
+            if !this.state_neg.contains(&item.0) {
+                return Poll::Ready(Some(item));
+            }
+        }
+
+        // Done
+        Poll::Ready(None)
     }
 }
