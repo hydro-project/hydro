067816a3e0decaa2e44f3a30bf6d490e9cd96ed5
Mingwei Samuel <mingwes@amazon.com>
2025-11-20 16:09:01 -0800

    flatten, fold (actually working)

--- dfir_rs/src/compiled/pull/mod.rs
+++ dfir_rs/src/compiled/pull/mod.rs
@@ -37,6 +37,9 @@ pub use sort_by_key::SortByKey;
 mod flat_map;
 pub use flat_map::FlatMap;
 
+mod flatten;
+pub use flatten::Flatten;
+
 mod lattice_bimorphism;
 pub use lattice_bimorphism::LatticeBimorphismStream;
 
