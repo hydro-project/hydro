1a6913782f1179090db978b449b3794881f0c379
Mingwei Samuel <mingwes@amazon.com>
2025-12-05 11:29:10 -0800

    cleanup symmetric_hash_join bench [ci-bench]

--- benches/benches/symmetric_hash_join.rs
+++ benches/benches/symmetric_hash_join.rs
@@ -1,3 +1,4 @@
+use std::future::Future;
 use std::hint::black_box;
 use std::pin::pin;
 use std::task::{Context, Poll, Waker};
@@ -9,6 +10,38 @@ use rand::SeedableRng;
 use rand::distributions::Distribution;
 use rand::rngs::StdRng;
 
+/// Helper function to run a symmetric hash join benchmark and consume all results
+#[inline(always)]
+fn run_join_benchmark<K, V1, V2>(
+    lhs: impl IntoIterator<Item = (K, V1)>,
+    rhs: impl IntoIterator<Item = (K, V2)>,
+) where
+    K: std::hash::Hash + Eq + Clone,
+    V1: Clone + Eq,
+    V2: Clone + Eq,
+{
+    let (mut lhs_state, mut rhs_state) =
+        black_box((HalfSetJoinState::default(), HalfSetJoinState::default()));
+    let join = symmetric_hash_join_into_stream(
+        black_box(futures::stream::iter(lhs)),
+        black_box(futures::stream::iter(rhs)),
+        &mut lhs_state,
+        &mut rhs_state,
+        false,
+    );
+    let join = pin!(join);
+    let Poll::Ready(join) = Future::poll(join, &mut Context::from_waker(Waker::noop())) else {
+        panic!()
+    };
+
+    let mut join = pin!(join);
+    while let Poll::Ready(Some(item)) =
+        Stream::poll_next(join.as_mut(), &mut Context::from_waker(Waker::noop()))
+    {
+        black_box(item);
+    }
+}
+
 fn ops(c: &mut Criterion) {
     let mut rng = StdRng::from_entropy();
 
@@ -16,87 +49,21 @@ fn ops(c: &mut Criterion) {
         let lhs: Vec<_> = (0..3000).map(|v| (v, ())).collect();
         let rhs: Vec<_> = (0..3000).map(|v| (v + 50000, ())).collect();
 
-        b.iter(|| {
-            let (mut lhs_state, mut rhs_state) =
-                black_box((HalfSetJoinState::default(), HalfSetJoinState::default()));
-            let join = symmetric_hash_join_into_stream(
-                black_box(futures::stream::iter(lhs.iter().cloned())),
-                black_box(futures::stream::iter(rhs.iter().cloned())),
-                &mut lhs_state,
-                &mut rhs_state,
-                false,
-            );
-            let join = pin!(join);
-            let Poll::Ready(join) = Future::poll(join, &mut Context::from_waker(Waker::noop()))
-            else {
-                panic!()
-            };
-
-            let mut join = pin!(join);
-            while let Poll::Ready(Some(item)) =
-                Stream::poll_next(join.as_mut(), &mut Context::from_waker(Waker::noop()))
-            {
-                black_box(item);
-            }
-        });
+        b.iter(|| run_join_benchmark(lhs.iter().cloned(), rhs.iter().cloned()));
     });
 
     c.bench_function("symmetric_hash_join/match_keys_diff_values", |b| {
         let lhs: Vec<_> = (0..3000).map(|v| (v, v)).collect();
         let rhs: Vec<_> = (0..3000).map(|v| (v, v + 50000)).collect();
 
-        b.iter(|| {
-            let (mut lhs_state, mut rhs_state) =
-                black_box((HalfSetJoinState::default(), HalfSetJoinState::default()));
-            let join = symmetric_hash_join_into_stream(
-                black_box(futures::stream::iter(lhs.iter().cloned())),
-                black_box(futures::stream::iter(rhs.iter().cloned())),
-                &mut lhs_state,
-                &mut rhs_state,
-                false,
-            );
-            let join = pin!(join);
-            let Poll::Ready(join) = Future::poll(join, &mut Context::from_waker(Waker::noop()))
-            else {
-                panic!()
-            };
-
-            let mut join = pin!(join);
-            while let Poll::Ready(Some(item)) =
-                Stream::poll_next(join.as_mut(), &mut Context::from_waker(Waker::noop()))
-            {
-                black_box(item);
-            }
-        });
+        b.iter(|| run_join_benchmark(lhs.iter().cloned(), rhs.iter().cloned()));
     });
 
     c.bench_function("symmetric_hash_join/match_keys_same_values", |b| {
         let lhs: Vec<_> = (0..3000).map(|v| (v, v)).collect();
         let rhs: Vec<_> = (0..3000).map(|v| (v, v)).collect();
 
-        b.iter(|| {
-            let (mut lhs_state, mut rhs_state) =
-                black_box((HalfSetJoinState::default(), HalfSetJoinState::default()));
-            let join = symmetric_hash_join_into_stream(
-                black_box(futures::stream::iter(lhs.iter().cloned())),
-                black_box(futures::stream::iter(rhs.iter().cloned())),
-                &mut lhs_state,
-                &mut rhs_state,
-                false,
-            );
-            let join = pin!(join);
-            let Poll::Ready(join) = Future::poll(join, &mut Context::from_waker(Waker::noop()))
-            else {
-                panic!()
-            };
-
-            let mut join = pin!(join);
-            while let Poll::Ready(Some(item)) =
-                Stream::poll_next(join.as_mut(), &mut Context::from_waker(Waker::noop()))
-            {
-                black_box(item);
-            }
-        });
+        b.iter(|| run_join_benchmark(lhs.iter().cloned(), rhs.iter().cloned()));
     });
 
     c.bench_function(
@@ -112,29 +79,7 @@ fn ops(c: &mut Criterion) {
                 .map(|v| (dist.sample(&mut rng) as usize, v + 8000))
                 .collect();
 
-            b.iter(|| {
-                let (mut lhs_state, mut rhs_state) =
-                    black_box((HalfSetJoinState::default(), HalfSetJoinState::default()));
-                let join = symmetric_hash_join_into_stream(
-                    black_box(futures::stream::iter(lhs.iter().cloned())),
-                    black_box(futures::stream::iter(rhs.iter().cloned())),
-                    &mut lhs_state,
-                    &mut rhs_state,
-                    false,
-                );
-                let join = pin!(join);
-                let Poll::Ready(join) = Future::poll(join, &mut Context::from_waker(Waker::noop()))
-                else {
-                    panic!()
-                };
-
-                let mut join = pin!(join);
-                while let Poll::Ready(Some(item)) =
-                    Stream::poll_next(join.as_mut(), &mut Context::from_waker(Waker::noop()))
-                {
-                    black_box(item);
-                }
-            });
+            b.iter(|| run_join_benchmark(lhs.iter().cloned(), rhs.iter().cloned()));
         },
     );
 
@@ -151,29 +96,7 @@ fn ops(c: &mut Criterion) {
                 .map(|v| (dist.sample(&mut rng) as usize, v + 8000))
                 .collect();
 
-            b.iter(|| {
-                let (mut lhs_state, mut rhs_state) =
-                    black_box((HalfSetJoinState::default(), HalfSetJoinState::default()));
-                let join = symmetric_hash_join_into_stream(
-                    black_box(futures::stream::iter(lhs.iter().cloned())),
-                    black_box(futures::stream::iter(rhs.iter().cloned())),
-                    &mut lhs_state,
-                    &mut rhs_state,
-                    false,
-                );
-                let join = pin!(join);
-                let Poll::Ready(join) = Future::poll(join, &mut Context::from_waker(Waker::noop()))
-                else {
-                    panic!()
-                };
-
-                let mut join = pin!(join);
-                while let Poll::Ready(Some(item)) =
-                    Stream::poll_next(join.as_mut(), &mut Context::from_waker(Waker::noop()))
-                {
-                    black_box(item);
-                }
-            });
+            b.iter(|| run_join_benchmark(lhs.iter().cloned(), rhs.iter().cloned()));
         },
     );
 }
