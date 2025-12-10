6351af9f1014506c5f976b025d0c20b430cb67af
Mingwei Samuel <mingwes@amazon.com>
2025-12-10 13:11:17 -0800

    2179-on-mac

--- hydro_lang/src/tests/hydro_deploy.rs
+++ hydro_lang/src/tests/hydro_deploy.rs
@@ -39,8 +39,8 @@ pub fn distributed_echo<'a>(
 ) -> (
     ExternalBincodeSink<u32>,
     ExternalBincodeStream<u32, NoOrder>,
-    ExternalBincodeStream<(MemberId<C2>, MembershipEvent), NoOrder>,
-    ExternalBincodeStream<(MemberId<C3>, MembershipEvent), NoOrder>,
+    // ExternalBincodeStream<(MemberId<C2>, MembershipEvent), NoOrder>,
+    // ExternalBincodeStream<(MemberId<C3>, MembershipEvent), NoOrder>,
 ) {
     let (tx, rx) = p1.source_external_bincode(external);
 
@@ -58,23 +58,23 @@ pub fn distributed_echo<'a>(
         .map(q!(|n| n + 1))
         .send_bincode_external(external);
 
-    let ems_c2 = p1
-        .source_cluster_members(c2)
-        .entries()
-        .send_bincode_external(external);
+    // let ems_c2 = p1
+    //     .source_cluster_members(c2)
+    //     .entries()
+    //     .send_bincode_external(external);
 
-    let ems_c3 = p1
-        .source_cluster_members(c3)
-        .entries()
-        .send_bincode_external(external);
+    // let ems_c3 = p1
+    //     .source_cluster_members(c3)
+    //     .entries()
+    //     .send_bincode_external(external);
 
-    (tx, rx, ems_c2, ems_c3)
+    (tx, rx) //, ems_c2, ems_c3)
 }
-
-const CLUSTER_SIZE: usize = 3;
 
 #[tokio::test]
 async fn docker() {
+    const CLUSTER_SIZE: usize = 1;
+
     telemetry::initialize_tracing_with_directive("trace,hyper=off");
 
     let network = DockerNetwork::new("distributed_echo_test".to_string());
@@ -88,7 +88,7 @@ async fn docker() {
     let c3 = builder.cluster();
     let p4 = builder.process();
     let p5 = builder.process();
-    let (external_sink, external_stream, ems_c2, ems_c3) =
+    let (external_sink, external_stream) =
         distributed_echo(&external, &p1, &c2, &c3, &p4, &p5);
 
     let config = vec![
@@ -118,31 +118,31 @@ async fn docker() {
     deployment.start(&nodes).await.unwrap();
 
     let mut external_sink = nodes.connect(external_sink).await;
-    let mut external_stream = nodes.connect(external_stream).await;
 
-    let mut ems_c2_stream = nodes.connect(ems_c2).await;
-    let mut ems_c3_stream = nodes.connect(ems_c3).await;
+    // let mut ems_c2_stream = nodes.connect(ems_c2).await;
+    // let mut ems_c3_stream = nodes.connect(ems_c3).await;
 
-    println!("waiting for c2 events");
-    {
-        let mut events = Vec::new();
-        events.push(ems_c2_stream.next().await.unwrap());
-        println!("got 1 c2 events");
-        events.push(ems_c2_stream.next().await.unwrap());
-        println!("got 2 c2 events");
-    }
+    // println!("waiting for c2 events");
+    // {
+    //     let mut events = Vec::new();
+    //     events.push(ems_c2_stream.next().await.unwrap());
+    //     println!("got 1 c2 events");
+    //     events.push(ems_c2_stream.next().await.unwrap());
+    //     println!("got 2 c2 events");
+    // }
 
-    println!("waiting for c3 events");
-    {
-        let mut events = Vec::new();
-        events.push(ems_c3_stream.next().await.unwrap());
-        println!("got 1 c3 events");
-        events.push(ems_c3_stream.next().await.unwrap());
-        println!("got 2 c3 events");
-    }
+    // println!("waiting for c3 events");
+    // {
+    //     let mut events = Vec::new();
+    //     events.push(ems_c3_stream.next().await.unwrap());
+    //     println!("got 1 c3 events");
+    //     events.push(ems_c3_stream.next().await.unwrap());
+    //     println!("got 2 c3 events");
+    // }
 
     println!("sending 0");
     external_sink.send(0).await.unwrap();
+    let mut external_stream = nodes.connect(external_stream).await;
     assert_eq!(external_stream.next().await.unwrap(), 5);
 
     println!("sending 1");
@@ -163,186 +163,190 @@ async fn docker() {
     println!("success");
 }
 
-#[tokio::test]
-async fn localhost() {
-    let mut deployment = Deployment::new();
+// #[tokio::test]
+// async fn localhost() {
+//     const CLUSTER_SIZE: usize = 3;
 
-    let builder = FlowBuilder::new();
-    let external = builder.external();
-    let p1 = builder.process();
-    let c2 = builder.cluster();
-    let c3 = builder.cluster();
-    let p4 = builder.process();
-    let p5 = builder.process();
-    let (external_sink, external_stream, ems_c2, ems_c3) =
-        distributed_echo(&external, &p1, &c2, &c3, &p4, &p5);
+//     let mut deployment = Deployment::new();
 
-    let nodes = builder
-        .with_process(&p1, deployment.Localhost())
-        .with_cluster(&c2, (0..CLUSTER_SIZE).map(|_| deployment.Localhost()))
-        .with_cluster(&c3, (0..CLUSTER_SIZE).map(|_| deployment.Localhost()))
-        .with_process(&p4, deployment.Localhost())
-        .with_process(&p5, deployment.Localhost())
-        .with_external(&external, deployment.Localhost())
-        .deploy(&mut deployment);
+//     let builder = FlowBuilder::new();
+//     let external = builder.external();
+//     let p1 = builder.process();
+//     let c2 = builder.cluster();
+//     let c3 = builder.cluster();
+//     let p4 = builder.process();
+//     let p5 = builder.process();
+//     let (external_sink, external_stream, ems_c2, ems_c3) =
+//         distributed_echo(&external, &p1, &c2, &c3, &p4, &p5);
 
-    deployment.deploy().await.unwrap();
-    deployment.start().await.unwrap();
+//     let nodes = builder
+//         .with_process(&p1, deployment.Localhost())
+//         .with_cluster(&c2, (0..CLUSTER_SIZE).map(|_| deployment.Localhost()))
+//         .with_cluster(&c3, (0..CLUSTER_SIZE).map(|_| deployment.Localhost()))
+//         .with_process(&p4, deployment.Localhost())
+//         .with_process(&p5, deployment.Localhost())
+//         .with_external(&external, deployment.Localhost())
+//         .deploy(&mut deployment);
 
-    let mut external_sink = nodes.connect(external_sink).await;
-    let mut external_stream = nodes.connect(external_stream).await;
+//     deployment.deploy().await.unwrap();
+//     deployment.start().await.unwrap();
 
-    let mut ems_c2_stream = nodes.connect(ems_c2).await;
-    let mut ems_c3_stream = nodes.connect(ems_c3).await;
+//     let mut external_sink = nodes.connect(external_sink).await;
+//     let mut external_stream = nodes.connect(external_stream).await;
 
-    println!("waiting for c2 events");
-    {
-        let mut events = Vec::new();
-        events.push(ems_c2_stream.next().await.unwrap());
-        println!("got 1 c2 events");
-        events.push(ems_c2_stream.next().await.unwrap());
-        println!("got 2 c2 events");
-    }
+//     let mut ems_c2_stream = nodes.connect(ems_c2).await;
+//     let mut ems_c3_stream = nodes.connect(ems_c3).await;
 
-    println!("waiting for c3 events");
-    {
-        let mut events = Vec::new();
-        events.push(ems_c3_stream.next().await.unwrap());
-        println!("got 1 c3 events");
-        events.push(ems_c3_stream.next().await.unwrap());
-        println!("got 2 c3 events");
-    }
+//     println!("waiting for c2 events");
+//     {
+//         let mut events = Vec::new();
+//         events.push(ems_c2_stream.next().await.unwrap());
+//         println!("got 1 c2 events");
+//         events.push(ems_c2_stream.next().await.unwrap());
+//         println!("got 2 c2 events");
+//     }
 
-    println!("sending 0");
-    external_sink.send(0).await.unwrap();
-    assert_eq!(external_stream.next().await.unwrap(), 5);
+//     println!("waiting for c3 events");
+//     {
+//         let mut events = Vec::new();
+//         events.push(ems_c3_stream.next().await.unwrap());
+//         println!("got 1 c3 events");
+//         events.push(ems_c3_stream.next().await.unwrap());
+//         println!("got 2 c3 events");
+//     }
 
-    println!("sending 1");
+//     println!("sending 0");
+//     external_sink.send(0).await.unwrap();
+//     assert_eq!(external_stream.next().await.unwrap(), 5);
 
-    external_sink.send(1).await.unwrap();
-    assert_eq!(external_stream.next().await.unwrap(), 6);
+//     println!("sending 1");
 
-    external_sink.send(2).await.unwrap();
-    assert_eq!(external_stream.next().await.unwrap(), 7);
+//     external_sink.send(1).await.unwrap();
+//     assert_eq!(external_stream.next().await.unwrap(), 6);
 
-    external_sink.send(3).await.unwrap();
-    assert_eq!(external_stream.next().await.unwrap(), 8);
+//     external_sink.send(2).await.unwrap();
+//     assert_eq!(external_stream.next().await.unwrap(), 7);
 
-    deployment.stop().await.unwrap();
+//     external_sink.send(3).await.unwrap();
+//     assert_eq!(external_stream.next().await.unwrap(), 8);
 
-    println!("success");
-}
+//     deployment.stop().await.unwrap();
 
-#[ignore]
-#[tokio::test(flavor = "multi_thread")]
-async fn distributed_echo_aws() {
-    let mut deployment: Deployment = Deployment::new();
+//     println!("success");
+// }
 
-    let builder = FlowBuilder::new();
-    let external = builder.external();
-    let p1 = builder.process();
-    let c2 = builder.cluster();
-    let c3 = builder.cluster();
-    let p4 = builder.process();
-    let p5 = builder.process();
-    let (external_sink, external_stream, ems_c2, ems_c3) =
-        distributed_echo(&external, &p1, &c2, &c3, &p4, &p5);
+// #[ignore]
+// #[tokio::test(flavor = "multi_thread")]
+// async fn distributed_echo_aws() {
+//     const CLUSTER_SIZE: usize = 3;
 
-    let network = Arc::new(RwLock::new(AwsNetwork::new("us-east-1", None)));
+//     let mut deployment: Deployment = Deployment::new();
 
-    let nodes = builder
-        .with_process(
-            &p1,
-            deployment.AwsEc2Host()
-                        .region("us-east-1")
-                        .instance_type("t3.micro")
-                        .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
-                        .network(network.clone())
-                        .add(),
-        )
-        .with_cluster(
-            &c2,
-            (0..CLUSTER_SIZE).map(|_| {
-                deployment.AwsEc2Host()
-                        .region("us-east-1")
-                        .instance_type("t3.micro")
-                        .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
-                        .network(network.clone())
-                        .add()
-            }),
-        )
-        .with_cluster(
-            &c3,
-            (0..CLUSTER_SIZE).map(|_| {
-                deployment.AwsEc2Host()
-                        .region("us-east-1")
-                        .instance_type("t3.micro")
-                        .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
-                        .network(network.clone())
-                        .add()
-            }),
-        )
-        .with_process(
-            &p4,
-            deployment.AwsEc2Host()
-                        .region("us-east-1")
-                        .instance_type("t3.micro")
-                        .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
-                        .network(network.clone())
-                        .add(),
-        )
-        .with_process(
-            &p5,
-            deployment.AwsEc2Host()
-                        .region("us-east-1")
-                        .instance_type("t3.micro")
-                        .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
-                        .network(network.clone())
-                        .add(),
-        )
-        .with_external(&external, deployment.Localhost())
-        .deploy(&mut deployment);
+//     let builder = FlowBuilder::new();
+//     let external = builder.external();
+//     let p1 = builder.process();
+//     let c2 = builder.cluster();
+//     let c3 = builder.cluster();
+//     let p4 = builder.process();
+//     let p5 = builder.process();
+//     let (external_sink, external_stream, ems_c2, ems_c3) =
+//         distributed_echo(&external, &p1, &c2, &c3, &p4, &p5);
 
-    deployment.deploy().await.unwrap();
+//     let network = Arc::new(RwLock::new(AwsNetwork::new("us-east-1", None)));
 
-    let mut external_sink = nodes.connect(external_sink).await;
-    let mut external_stream = nodes.connect(external_stream).await;
+//     let nodes = builder
+//         .with_process(
+//             &p1,
+//             deployment.AwsEc2Host()
+//                         .region("us-east-1")
+//                         .instance_type("t3.micro")
+//                         .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
+//                         .network(network.clone())
+//                         .add(),
+//         )
+//         .with_cluster(
+//             &c2,
+//             (0..CLUSTER_SIZE).map(|_| {
+//                 deployment.AwsEc2Host()
+//                         .region("us-east-1")
+//                         .instance_type("t3.micro")
+//                         .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
+//                         .network(network.clone())
+//                         .add()
+//             }),
+//         )
+//         .with_cluster(
+//             &c3,
+//             (0..CLUSTER_SIZE).map(|_| {
+//                 deployment.AwsEc2Host()
+//                         .region("us-east-1")
+//                         .instance_type("t3.micro")
+//                         .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
+//                         .network(network.clone())
+//                         .add()
+//             }),
+//         )
+//         .with_process(
+//             &p4,
+//             deployment.AwsEc2Host()
+//                         .region("us-east-1")
+//                         .instance_type("t3.micro")
+//                         .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
+//                         .network(network.clone())
+//                         .add(),
+//         )
+//         .with_process(
+//             &p5,
+//             deployment.AwsEc2Host()
+//                         .region("us-east-1")
+//                         .instance_type("t3.micro")
+//                         .ami("ami-0e95a5e2743ec9ec9") // Amazon Linux 2
+//                         .network(network.clone())
+//                         .add(),
+//         )
+//         .with_external(&external, deployment.Localhost())
+//         .deploy(&mut deployment);
 
-    let mut ems_c2_stream = nodes.connect(ems_c2).await;
-    let mut ems_c3_stream = nodes.connect(ems_c3).await;
+//     deployment.deploy().await.unwrap();
 
-    println!("waiting for c2 events");
-    {
-        let mut events = Vec::new();
-        events.push(ems_c2_stream.next().await.unwrap());
-        println!("got 1 c2 events");
-        events.push(ems_c2_stream.next().await.unwrap());
-        println!("got 2 c2 events");
-    }
+//     let mut external_sink = nodes.connect(external_sink).await;
+//     let mut external_stream = nodes.connect(external_stream).await;
 
-    println!("waiting for c3 events");
-    {
-        let mut events = Vec::new();
-        events.push(ems_c3_stream.next().await.unwrap());
-        println!("got 1 c3 events");
-        events.push(ems_c3_stream.next().await.unwrap());
-        println!("got 2 c3 events");
-    }
+//     let mut ems_c2_stream = nodes.connect(ems_c2).await;
+//     let mut ems_c3_stream = nodes.connect(ems_c3).await;
 
-    println!("sending 0");
-    external_sink.send(0).await.unwrap();
-    assert_eq!(external_stream.next().await.unwrap(), 5);
+//     println!("waiting for c2 events");
+//     {
+//         let mut events = Vec::new();
+//         events.push(ems_c2_stream.next().await.unwrap());
+//         println!("got 1 c2 events");
+//         events.push(ems_c2_stream.next().await.unwrap());
+//         println!("got 2 c2 events");
+//     }
 
-    println!("sending 1");
+//     println!("waiting for c3 events");
+//     {
+//         let mut events = Vec::new();
+//         events.push(ems_c3_stream.next().await.unwrap());
+//         println!("got 1 c3 events");
+//         events.push(ems_c3_stream.next().await.unwrap());
+//         println!("got 2 c3 events");
+//     }
 
-    external_sink.send(1).await.unwrap();
-    assert_eq!(external_stream.next().await.unwrap(), 6);
+//     println!("sending 0");
+//     external_sink.send(0).await.unwrap();
+//     assert_eq!(external_stream.next().await.unwrap(), 5);
 
-    external_sink.send(2).await.unwrap();
-    assert_eq!(external_stream.next().await.unwrap(), 7);
+//     println!("sending 1");
 
-    external_sink.send(3).await.unwrap();
-    assert_eq!(external_stream.next().await.unwrap(), 8);
+//     external_sink.send(1).await.unwrap();
+//     assert_eq!(external_stream.next().await.unwrap(), 6);
 
-    deployment.stop().await.unwrap();
-}
+//     external_sink.send(2).await.unwrap();
+//     assert_eq!(external_stream.next().await.unwrap(), 7);
+
+//     external_sink.send(3).await.unwrap();
+//     assert_eq!(external_stream.next().await.unwrap(), 8);
+
+//     deployment.stop().await.unwrap();
+// }
