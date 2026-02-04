hydro-project/hydro

commit 22d10a22aa3c55eea0ae41c96be464a9f306cb43

fix(dfir_lang): ensure `source_iter` always drains on first tick (#2432)

dfir_lang/src/graph/ops/source_iter.rs
@@ -45,7 +45,7 @@ pub const SOURCE_ITER: OperatorConstraints = OperatorConstraints {
         let iter = &arguments[0];
         let iter_ident = wc.make_ident("iter");
         let write_prologue = quote_spanned! {op_span=>
-            let mut #iter_ident = {
+            let mut #iter_ident: Option<_> = {
                 #[inline(always)]
                 fn check_iter<IntoIter, Item>(into_iter: IntoIter) -> impl ::std::iter::Iterator<Item = Item>
                 where
@@ -53,11 +53,17 @@ pub const SOURCE_ITER: OperatorConstraints = OperatorConstraints {
                 {
                     ::std::iter::IntoIterator::into_iter(into_iter)
                 }
-                check_iter(#iter)
+                Some(check_iter(#iter))
             };
         };
         let write_iterator = quote_spanned! {op_span=>
-            let #ident = #root::futures::stream::iter(#iter_ident.by_ref());
+            let #ident = #root::futures::stream::iter(
+                ::std::iter::Iterator::flatten(
+                    ::std::iter::IntoIterator::into_iter(
+                        ::std::option::Option::take(&mut #iter_ident)
+                    )
+                )
+            );
         };
         Ok(OperatorWriteOutput {
             write_prologue,

hydro_lang/src/live_collections/keyed_stream/mod.rs
@@ -2256,9 +2256,9 @@ mod tests {
         let next_watermark = watermark.clone().map(q!(|v| v + 1));
         watermark_complete_cycle.complete_next_tick(next_watermark);
 
-        let tick_triggered_input = node
-            .source_iter(q!([(3, 103)]))
-            .batch(&node_tick, nondet!(/** test */))
+        let tick_triggered_input = node_tick
+            .singleton(q!((3, 103)))
+            .into_stream()
             .filter_if_some(
                 tick_trigger
                     .clone()

commit d74a3097af2f78099d08c7a93d2f0ed7d75418e3

fix(dfir_lang): fix `source_iter` performance regression from #2432 [ci-bench]

dfir_lang/src/graph/ops/source_iter.rs
@@ -45,7 +45,7 @@ pub const SOURCE_ITER: OperatorConstraints = OperatorConstraints {
         let iter = &arguments[0];
         let iter_ident = wc.make_ident("iter");
         let write_prologue = quote_spanned! {op_span=>
-            let mut #iter_ident: Option<_> = {
+            let mut #iter_ident = {
                 #[inline(always)]
                 fn check_iter<IntoIter, Item>(into_iter: IntoIter) -> impl ::std::iter::Iterator<Item = Item>
                 where
@@ -53,21 +53,22 @@ pub const SOURCE_ITER: OperatorConstraints = OperatorConstraints {
                 {
                     ::std::iter::IntoIterator::into_iter(into_iter)
                 }
-                Some(check_iter(#iter))
+                check_iter(#iter)
             };
         };
         let write_iterator = quote_spanned! {op_span=>
-            let #ident = #root::futures::stream::iter(
-                ::std::iter::Iterator::flatten(
-                    ::std::iter::IntoIterator::into_iter(
-                        ::std::option::Option::take(&mut #iter_ident)
-                    )
-                )
-            );
+            let #ident = #root::futures::stream::iter(&mut #iter_ident);
         };
+
+        // Ensure contents are always drained on the first tick.
+        let write_iterator_after = quote_spanned! {op_span=>
+            ::std::iter::Iterator::for_each(&mut #iter_ident, ::std::mem::drop);
+        };
+
         Ok(OperatorWriteOutput {
             write_prologue,
             write_iterator,
+            write_iterator_after,
             ..Default::default()
         })
     },