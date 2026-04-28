5396a8ae2c4bfc5529a60cc4892b20c8d91e99d7
David Chu <chudc@amazon.com>
2026-04-28 15:08:56 -0700

    ReduceKeyedWatermark generated DFIR was broken for non-Copy watermarks

--- hydro_lang/src/compile/ir/mod.rs
+++ hydro_lang/src/compile/ir/mod.rs
@@ -3876,8 +3876,8 @@ impl HydroNode {
                                                                     return;
                                                                 }
                                                             }
-                                                            *opt_curr_watermark = opt_watermark;
                                                             map.retain(|k, _| *k >= watermark);
+                                                            *opt_curr_watermark = Some(watermark);
                                                         }
                                                     }
                                                 })
